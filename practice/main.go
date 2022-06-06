9ece29b86794d0f255ae89cb63cb8d0a967fccb2
Giovanni Fulco <giovanni.fulco@gmail.com>
2022-06-06 13:21:23 +0200

    print with some decimals

--- practice/main.go
+++ practice/main.go
@@ -14,6 +14,7 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	floatSum := f1 + f2 + f3
 	fmt.Println("Float sum:", floatSum)
+	fmt.Printf("Float sum (precision 2) %.2f\n", floatSum)
 
 	floatSum = math.Round(floatSum*100) / 100
 	fmt.Println("The sum is now", floatSum)
