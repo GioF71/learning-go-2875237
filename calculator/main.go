5d13b8cab00c55ade94775040d9cc5193955e836
GioF_71 <giovanni.fulco@gmail.com>
2022-06-06 18:42:05 +0200

    a more sophisticated calculator

--- calculator/main.go
+++ calculator/main.go
@@ -8,17 +8,42 @@ import (
 	"strings"
 )
 
-func get_from_reader(reader *bufio.Reader, valueName string) float64 {
-	fmt.Printf("Enter %s: ", valueName)
+type OperationType func(float64, float64) float64
+
+func sum(v1 float64, v2 float64) float64 {
+	return v1 + v2
+}
+
+func subtract(v1 float64, v2 float64) float64 {
+	return v1 - v2
+}
+
+func multiply(v1 float64, v2 float64) float64 {
+	return v1 * v2
+}
+
+func divide(v1 float64, v2 float64) float64 {
+	return v1 / v2
+}
+
+func getUserInput(inputName string) string {
+	reader := bufio.NewReader(os.Stdin)
 	var inputString string
-	var theValue float64 = 0.0
 	var theError error = nil
 
 	inputString, theError = reader.ReadString('\n')
 	if theError != nil {
 		fmt.Println(theError)
-		panic(fmt.Sprintf("Cannot read value for [%s]", valueName))
+		panic(fmt.Sprintf("Cannot read user input for [%s]", inputName))
 	}
+	return strings.TrimSpace(inputString)
+}
+
+func getValue(valueName string) float64 {
+	fmt.Printf("Enter %s: ", valueName)
+	inputString := getUserInput(valueName)
+	var theError error = nil
+	var theValue float64 = 0.0
 	theValue, theError = strconv.ParseFloat(strings.TrimSpace(inputString), 64)
 	if theError != nil {
 		fmt.Println(theError)
@@ -27,15 +52,48 @@ func get_from_reader(reader *bufio.Reader, valueName string) float64 {
 	return theValue
 }
 
-func get(valueName string) float64 {
-	reader := bufio.NewReader(os.Stdin)
-	result := get_from_reader(reader, valueName)
-	return result
+func getOperation(validOperationArray []string) string {
+	valueName := "operation"
+	fmt.Printf("Enter %s: ", valueName)
+	inputString := getUserInput(valueName)
+	if len(inputString) != 1 {
+		panic(fmt.Sprintf("Operation length is %d, must be 1", len(inputString)))
+	}
+	success := false
+	for _, validEntry := range validOperationArray {
+		if strings.Compare(validEntry, inputString) == 0 {
+			success = true
+			break
+		}
+	}
+	if !success {
+		panic(fmt.Sprintf("Operation %s is not allowed", inputString))
+	}
+	return inputString
 }
 
 func main() {
+	operations := []string{"+", "-", "*", "/"}
+
+	opDescMap := make(map[string]string)
+	opDescMap[operations[0]] = "sum"
+	opDescMap[operations[1]] = "subtract"
+	opDescMap[operations[2]] = "multiply"
+	opDescMap[operations[3]] = "divide"
+
+	fmt.Println(operations)
+
+	opMap := make(map[string]OperationType)
+	opMap[operations[0]] = sum
+	opMap[operations[1]] = subtract
+	opMap[operations[2]] = multiply
+	opMap[operations[3]] = divide
+
 	fmt.Println("A simple calculator")
-	v1 := get("v1")
-	v2 := get("v2")
-	fmt.Printf("%f + %f = %f\n", v1, v2, v1+v2)
+	v1 := getValue("v1")
+	v2 := getValue("v2")
+	operation := getOperation(operations)
+	fmt.Printf("Selected operation is: [%s]\n", opDescMap[operation])
+	opType := opMap[operation]
+	fmt.Printf("%f + %f = %f\n", v1, v2, opType(v1, v2))
 }
